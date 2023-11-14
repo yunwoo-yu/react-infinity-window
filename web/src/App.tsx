9b9bb83fb20227b478550d771f21365edc9fa0e9
YunCow <skypnal12@gmail.com>
2023-11-15 00:36:17 +0900

    chore: temp

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
-import { MultipleList } from "react-infinity-window";
-
 function App() {
+  // 옵저버를 item마다 붙이는 건 성능 상 문제가 있을까? 리스트가 100개면 옵저버가 100개.. 음..
+  // 기존 react-window는 스크롤 높이를 계산해서 element를 생성한다. 그럼 이전 데이터는 어떻게 유지를 하지
+
   return (
     <>
       <div style={{ height: "100vh", backgroundColor: "#bfc928" }}>
