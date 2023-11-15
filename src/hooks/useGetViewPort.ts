c7085170eab19b4d96f345ba6b074152bccd38ed
YunCow <skypnal12@gmail.com>
2023-11-16 00:12:05 +0900

    feat: get viewPort height hook add

--- src/hooks/useGetViewPort.ts
+++ src/hooks/useGetViewPort.ts
@@ -1,5 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useGetViewPort = () => {
-  const [startIndex, setStartIndex] = useState(0);
+export const useGetViewPortHeight = () => {
+  const [viewPortHeight, setViewPortHeight] = useState(window.innerHeight);
+
+  const handleResize = () => {
+    const height = window.innerHeight;
+    setViewPortHeight(height);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize, { passive: true });
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return viewPortHeight;
 };
