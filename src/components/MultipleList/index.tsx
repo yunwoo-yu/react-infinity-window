c73ee1068a83af314d6fd657f0be79580b9ccad4
YunCow <skypnal12@gmail.com>
2023-11-18 00:15:02 +0900

    feat: dummy hooks test

--- src/components/MultipleList/index.tsx
+++ src/components/MultipleList/index.tsx
@@ -1,5 +1,13 @@
+import useGetScrollPos from "hooks/useGetScrollPos";
+import { useGetViewPortHeight } from "hooks/useGetViewPort";
 import React from "react";
 
 export const MultipleList = () => {
+  const scroll = useGetScrollPos();
+  const height = useGetViewPortHeight();
+
+  console.log("scroll", scroll);
+  console.log("height", height);
+
   return <div>MultipleList</div>;
 };
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,21 @@
+import { MultipleList } from "react-infinity-window";
+
+const TEST_DATA = [
+  { title: "title1", content: "content1" },
+  { title: "title2", content: "content2" },
+  { title: "title3", content: "content3" },
+  { title: "title4", content: "content4" },
+  { title: "title5", content: "content5" },
+  { title: "title6", content: "content6" },
+];
+
 function App() {
   // 옵저버를 item마다 붙이는 건 성능 상 문제가 있을까? 리스트가 100개면 옵저버가 100개.. 음..
   // 기존 react-window는 스크롤 높이를 계산해서 element를 생성한다. 그럼 이전 데이터는 어떻게 유지를 하지
 
   return (
     <>
+      <MultipleList />
       <div style={{ height: "100vh", backgroundColor: "#bfc928" }}>
         꽉 차는 컨테이너
       </div>
