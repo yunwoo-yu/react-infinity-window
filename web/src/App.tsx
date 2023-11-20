76ac67ad1b17714809efe487ed5fe58bdf1f4d32
YunCow <skypnal12@gmail.com>
2023-11-20 22:42:47 +0900

    feat: 가상화 성공 (최적화 & 동적 height 미적용)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useRef, useState } from "react";
 import { MultipleList } from "react-infinity-window";
 
 const TEST_DATA = [
@@ -10,12 +11,28 @@ const TEST_DATA = [
 ];
 
 function App() {
-  // 옵저버를 item마다 붙이는 건 성능 상 문제가 있을까? 리스트가 100개면 옵저버가 100개.. 음..
-  // 기존 react-window는 스크롤 높이를 계산해서 element를 생성한다. 그럼 이전 데이터는 어떻게 유지를 하지
+  const containerRef = useRef(null);
+  const itemHeight = 50;
+  const totalItems = 1000;
+  const bufferSize = 5;
+
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(bufferSize);
+
+  const handleScroll = useCallback(() => {
+    if (containerRef.current) {
+      const { scrollTop, clientHeight } = containerRef.current;
+      const start = Math.floor(scrollTop / itemHeight);
+      const end = start + Math.ceil(clientHeight / itemHeight) + bufferSize;
+
+      setStartIndex(Math.max(0, start));
+      setEndIndex(Math.min(totalItems, end));
+    }
+  }, []);
 
   return (
     <>
-      <MultipleList />
+      {/* <MultipleList />
       <div style={{ height: "100vh", backgroundColor: "#bfc928" }}>
         꽉 차는 컨테이너
       </div>
@@ -30,6 +47,37 @@ function App() {
       </div>
       <div style={{ height: "300px", backgroundColor: "#999999" }}>
         리스트 4
+      </div> */}
+      <div
+        ref={containerRef}
+        style={{ maxHeight: "400px", overflowY: "auto" }}
+        onScroll={handleScroll}
+      >
+        <div
+          style={{
+            height: `${itemHeight * totalItems}px`,
+            position: "relative",
+          }}
+        >
+          {Array.from({ length: endIndex - startIndex }, (_, index) => (
+            <div
+              key={startIndex + index}
+              style={{
+                position: "absolute",
+                top: `${(startIndex + index) * itemHeight}px`,
+                height: `${itemHeight}px`,
+                width: "100%",
+                border: "1px solid #ccc",
+                boxSizing: "border-box",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Item {startIndex + index}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
